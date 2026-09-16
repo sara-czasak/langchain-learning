8514d7e5a84f6551c933d9acf3824e27004845c3
Star_Wasp7272 <star.wasp7272@gmail.com>
2026-09-16 13:33:53 +0200

    start building front end

---
+++ weather_agent/web_app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+
+st.title("Playful Weather Agent")
+
+st.write("What can I help you with?")
+
+query_input_box = st.text_input(
+    label="Type your weather query..."
+)
+
+# TODO connect the front end to backend (main.py)
+# TODO figure out how to make a decent interface
+
+
